f475d56c525cbdd8eaa0450085de7c51989c6466
Abhishek Sharan <84129078+Abhishek-Sharan@users.noreply.github.com>
2024-10-04 10:22:23 +0530

    Add files via upload
    This PowerShell script can be used to set total retention period for log analytics tables. Changes can be done for multiple tables (comma separated) at once. 
    
    Note: We highly recommend you should review the PowerShell script thoroughly and do proper testing before executing it in production. We don't take any responsibility for the script.

---
+++ SetTableRetentionAtScale.ps1
@@ -0,0 +1,28 @@
+﻿# Function to set retention for a Log Analytics table
+function Set-LogAnalyticsRetention {
+    param (
+        [string]$ResourceGroupName,
+        [string]$WorkspaceName,
+        [string]$TableName,
+        [int]$TotalRetentionInDays
+    )
+
+    Update-AzOperationalInsightsTable -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName -TableName $TableName -TotalRetentionInDays $TotalRetentionInDays
+}
+
+# Prompt for user input
+$resourceGroupName = Read-Host "Enter the Resource Group Name"
+$workspaceName = Read-Host "Enter the Workspace Name"
+# Prompt for multiple table names
+$TableName = Read-Host "Enter the Table Names (comma-separated)"
+
+# Split the table names into an array
+$TableNameArray = $TableName -split ","
+
+$TotalRetentionInDays = Read-Host "Enter the Total Retention Period in Days"
+
+# Loop through each table name and set the retention period
+foreach ($TableName in $TableNameArray) {
+    Set-LogAnalyticsRetention -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -TableName $TableName.Trim() -TotalRetentionInDays $TotalRetentionInDays
+    Write-Host "Retention period set successfully for table $TableName in workspace $workspaceName."
+}
